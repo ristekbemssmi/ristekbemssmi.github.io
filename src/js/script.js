7be16f3632ee28fc21559a28c08890eb8fa259bf
MuhammadZakwanSakhiy <muhammadzakwansakhiy@gmail.com>
2026-03-26 07:57:34 +0700

    menambahkan visi, informasi, dan beasiswa serta beberapa responsive component

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,20 @@ window.onscroll = function() {
     const fixedNav = header.offsetTop;
 
     if(window.pageYOffset > fixedNav) {
-        header.classList.add("navbar-fixed");
+        header.classList.add("fixed");
+        header.classList.remove("absolute");
         container.classList.remove("sm:rounded-2xl");
+        container.classList.add("sm:rounded-b-2xl");
+        header.classList.remove("sm:px-8");
+        header.classList.add("sm:px-4");
+        header.classList.remove("sm:py-6");
     } else {
-        header.classList.remove("navbar-fixed");
+        header.classList.remove("fixed");
+        header.classList.add("absolute");
         container.classList.add("sm:rounded-2xl");
+        header.classList.add("sm:px-8");
+        header.classList.remove("sm:px-4");
+        header.classList.add("sm:py-6");
     }
 };
 
@@ -26,25 +35,25 @@ hamburger.addEventListener('click', function() {
 const informasi = document.getElementById('informasi-dropdown');
 const informasiMenu = document.getElementById('informasi-menu');
 informasi.addEventListener('mouseenter', function() {
-    informasiMenu.classList.remove('hidden');
+    informasiMenu.classList.remove('scale-0');
 });
 
 informasi.addEventListener('mouseleave', () => {
     informasiMenu.addEventListener('mouseenter', function() {
-        informasiMenu.classList.remove('hidden');
+        informasiMenu.classList.remove('scale-0');
     });
 });
 
 informasi.addEventListener('mouseleave', () => {
-    informasiMenu.classList.add('hidden');
+    informasiMenu.classList.add('scale-0');
 });
 
 informasiMenu.addEventListener('mouseleave', () => {
-    informasiMenu.classList.add('hidden');
+    informasiMenu.classList.add('scale-0');
 });
 
 informasi.addEventListener('click', () => {
-    informasiMenu.classList.toggle('hidden');
+    informasiMenu.classList.toggle('scale-0');
 });
 
 // Informasi Aktif Mobile
